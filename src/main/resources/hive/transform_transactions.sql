2cd3b904fdc3809ccd0514f92b379b7131b8fd30
xtutran <xtutran@gmail.com>
2016-01-24 22:13:36 +0800

    transform trans

---
+++ src/main/resources/hive/transform_transactions.sql
@@ -0,0 +1,21 @@
+USE cloudera;
+
+DROP TABLE IF EXISTS transformed_trans;
+CREATE EXTERNAL TABLE transformed_trans (
+	--id,chain,dept,category,company,brand,date,productsize,productmeasure,purchasequantity,purchaseamount
+	id BIGINT,
+	chain INT,
+	dept INT,
+	category INT,
+	company BIGINT,
+	brand BIGINT,
+	tdate STRING,
+	productsize INT,
+	productmeasure STRING,
+	purchasequantity INT,
+	purchaseamount DOUBLE
+)
+STORED AS SEQUENCEFILE
+LOCATION '/user/cloudera/rec_data/transformed_trans';
+
+FROM transactions trans INSERT OVERWRITE TABLE transformed_trans SELECT *;
