d58e655918a97967acd7ff55e352c22ffe56235b
xtutran <xtutran@gmail.com>
2016-02-09 23:37:38 +0800

    ddd

--- src/main/resources/hive/transform_transactions.sql
+++ src/main/resources/hive/transform_transactions.sql
@@ -17,5 +17,5 @@ CREATE EXTERNAL TABLE partitioned_trans (
 PARTITIONED BY(tdate String)
 STORED AS TEXTFILE
 LOCATION '/user/cloudera/rec_data/transformed_trans';
---AS
---SELECT id, chain, dept, category, company, brand, productsize, productmeasure, purchasequantity, purchaseamount, tdate FROM transactions;
+AS
+SELECT id, chain, dept, category, company, brand, productsize, productmeasure, purchasequantity, purchaseamount, tdate FROM transactions;
